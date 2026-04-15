67166b163f391c49db29e7d2fbff7dd2b2c37684
matheus29lfy <matheus29lfy@gmail.com>
2026-04-15 10:11:39 -0300

    Ajuste bd para subir projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,17 +11,21 @@ import { Document } from '../src/modules/documents/entities/document.entity'
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db', // Nome do serviço no docker-compose
+      host: 'localhost', // Nome do serviço no docker-compose
       port: 5432,
       username: 'user',
       password: 'pass',
       database: 'med_db',
       entities: [Order, Exam, Document],
-      synchronize: true, // Apenas para desenvolvimento (gera as tabelas automaticamente)
+      synchronize: true, 
+      autoLoadEntities: true, // Adicione isso
+      retryAttempts: 10,     // Aumenta as tentativas
+      retryDelay: 3000,      // Espera 3 segundos entre elas
     }),
     OrdersModule,
     DocumentsModule,
     ExamsModule,
   ],
 })
+
 export class AppModule {}
