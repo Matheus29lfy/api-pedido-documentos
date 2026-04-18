c54cdac79b5fc58554a33171b3f0a6530ba25aef
matheus29lfy <matheus29lfy@gmail.com>
2026-04-18 13:30:23 -0300

    Remoção método não utilizado

--- src/modules/exams/exams.controller.ts
+++ src/modules/exams/exams.controller.ts
@@ -4,12 +4,7 @@ import { ExamsService } from './exams.service';
 @Controller('exames')
 export class ExamsController {
   constructor(private readonly examsService: ExamsService) {}
-
-  @Post()
-  async receiveArrival(@Body() arrivalDto: { AccessionNumber: string }) {
-    return this.examsService.handleArrival(arrivalDto);
-  }
-
+  
   @Get(':accessionNumber')
   async findOne(@Param('accessionNumber') accessionNumber: string) {
     return  await this.examsService.findOne(accessionNumber);
